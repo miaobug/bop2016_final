7874254e3c8a5b9177f9d2e16298f1e96ca250e9
Miao Rui <1200012426@pku.edu.cn>
2016-05-19 12:09:47 +0800

    整理了API KEY
    Signed-off-by: Miao Rui <1200012426@pku.edu.cn>

--- api/api.py
+++ api/api.py
@@ -1,10 +1,2 @@
 # -*- coding: utf-8 -*-
 
-
-f = open("key.txt")
-f2 = open("api_key.txt","w")
-
-f = f.readlines()
-
-for i in range(0, len(f), 13):
-    print >> f2, f[i], f[i+1][:-1], f[i+2], f[i+4][:-1], f[i+5], f[i+9]
