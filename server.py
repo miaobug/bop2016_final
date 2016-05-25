462023f9600709cb12fa4302b7386b8a3b8a5a37
FredyFang <510641426@qq.com>
2016-05-25 17:49:24 +0800

    temp version

--- server.py
+++ server.py
@@ -3,27 +3,55 @@ import Image
 import json
 
 def allPic():
+	picList = []
+	f = file('photos.json')
+	s = json.load(f)
+	for i in s:
+		picList.append(i["photoId"])
+	return picList
+
 
 
 def lastPic():
-
+	picList = []
+	f = file('photos.json')
+	s = json.load(f)
+	i = s[-1]
+	return i["phptoId"]
 
 def firstPic():	
-
+	picList = []
+	f = file('photos.json')
+	s = json.load(f)
+	i = s[0]
+	return i["phptoId"]
 
 def findLoc(placeName):
-
+	picList = []
+	f = file('photos.json')
+	s = json.load(f)
+	for i in s:
+		if i["location"] == placeName:
+			picList.append(i["photoId"])
+	return picList
 
 def findDate(dateName):
+	picList = []
+	f = file('photos.json')
+	s = json.load(f)
+	for i in s:
+		if i["date"] == dateName:
+			picList.append(i["photoId"])
+	return picList
 
 def parseJson(s):
 	#f = file('test.json')---------for test
 	#s = json.load(f)-----------for test
 	res = s["intents"][1]["intent"]
 
-	if(res == "show_last")
+	if res == "show_last":
 		img = lastPic()
-	elif(res == "show_first")
+	elif res == "show_first":
 		img = firstPic()
 	elif(res == "show_previous")
 
@@ -38,9 +66,9 @@ def parseJson(s):
 	elif(res == "filter_by_date_location")
 		imglist = allPic()
 		for ent in s["entities"]
-			if(ent["type"] == "location")
+			if ent["type"] == "location":
 				imglist = list(set(imglist).intersection(set( findLoc(ent["entity"]) )))
-			elif(ent["type"] == "builtin.datetime.date")
+			elif ent["type"] == "builtin.datetime.date":
 				imglist = list(set(imglist).intersection(set( findDate(ent["entity"]) )))
 
 	elif(res == "filter_by_trip")
@@ -48,7 +76,7 @@ def parseJson(s):
 	elif(res == "filter_by_tag")
 		imglist = []
 		for ent in s["entities"]
-			if(ent["type"] == "tag")
+			if ent["type"] == "tag":
 				imglist = findTag(ent["entity"])
 
 	elif(res == "remove_filter")
@@ -57,11 +85,11 @@ def parseJson(s):
 	elif(res == "add_tag")
 
 	elif(res == "rotate")
-		if(s["entities"][1]["entity"] == "右" or s["entities"][1]["entity"] == "顺时针")
+		if s["entities"][1]["entity"] == "右" or s["entities"][1]["entity"] == "顺时针":
 			#发送指令到浏览器
-		elif(s["entities"][1]["entity"] == "左" or s["entities"][1]["entity"] == "逆时针")
+		elif s["entities"][1]["entity"] == "左" or s["entities"][1]["entity"] == "逆时针":
 			#发送指令到浏览器
-	elif(res == "stop_auto_show")
+	elif(res == "stop_auto_show") 
 		#发送指令到浏览器
 	elif(res == "start_auto_show")
 		#发送指令到浏览器
@@ -69,6 +97,7 @@ def parseJson(s):
 
 	else
 		print "not in range"
-
+if __name__ == "__main__":
+    allPic()
 
 
