9e85306fad7ea8f1aff14d735d15704c431dd712
FredyFang <510641426@qq.com>
2016-05-25 19:25:24 +0800

    temp version

--- server.py
+++ server.py
@@ -53,47 +53,47 @@ def parseJson(s):
 		img = lastPic()
 	elif res == "show_first":
 		img = firstPic()
-	elif(res == "show_previous")
-
-	elif(res == "show_next")
-
-	elif(res == "show_n_by_m_per_page")
+	elif res == "show_previous":
+		print "show_previous"
+	elif res == "show_next":
+		print "show next"
+	elif res == "show_n_by_m_per_page":
 		#发送指令到浏览器
-	elif(res == "show_n_per_page")
+	elif res == "show_n_per_page":
 		#发送指令到浏览器
-	elif(res == "show_one_per_page")
+	elif res == "show_one_per_page":
 		#发送指令到浏览器
-	elif(res == "filter_by_date_location")
+	elif res == "filter_by_date_location":
 		imglist = allPic()
-		for ent in s["entities"]
+		for ent in s["entities"]:
 			if ent["type"] == "location":
 				imglist = list(set(imglist).intersection(set( findLoc(ent["entity"]) )))
 			elif ent["type"] == "builtin.datetime.date":
 				imglist = list(set(imglist).intersection(set( findDate(ent["entity"]) )))
 
-	elif(res == "filter_by_trip")
+	elif res == "filter_by_trip":
 
-	elif(res == "filter_by_tag")
+	elif res == "filter_by_tag":
 		imglist = []
-		for ent in s["entities"]
+		for ent in s["entities"]:
 			if ent["type"] == "tag":
 				imglist = findTag(ent["entity"])
 
-	elif(res == "remove_filter")
+	elif res == "remove_filter":
 		imglist = allPic()
 		#发送图片到浏览器
-	elif(res == "add_tag")
+	elif res == "add_tag":
 
-	elif(res == "rotate")
+	elif res == "rotate":
 		if s["entities"][1]["entity"] == "右" or s["entities"][1]["entity"] == "顺时针":
 			#发送指令到浏览器
 		elif s["entities"][1]["entity"] == "左" or s["entities"][1]["entity"] == "逆时针":
 			#发送指令到浏览器
-	elif(res == "stop_auto_show") 
+	elif res == "stop_auto_show":
 		#发送指令到浏览器
-	elif(res == "start_auto_show")
+	elif res == "start_auto_show":
 		#发送指令到浏览器
-	elif(res == "None")
+	elif res == "None":
 
 	else
 		print "not in range"
