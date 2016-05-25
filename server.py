7ea2e39bddf5ad8410fd9660e4545024b8a3dfff
FredyFang <510641426@qq.com>
2016-05-25 20:07:46 +0800

    temp version

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 #-*- coding:utf-8 -*-
 import Image
 import json
+dict = {"一" : 1, "二" : 2, "三" : 3, "四" :4}
 
 def allPic():
 	picList = []
@@ -8,6 +9,7 @@ def allPic():
 	s = json.load(f)
 	for i in s:
 		picList.append(i["photoId"])
+	#print picList
 	return picList
 
 
@@ -17,14 +19,15 @@ def lastPic():
 	f = file('photos.json')
 	s = json.load(f)
 	i = s[-1]
-	return i["phptoId"]
+	#print i["photoId"]
+	return i["photoId"]
 
 def firstPic():	
 	picList = []
 	f = file('photos.json')
 	s = json.load(f)
 	i = s[0]
-	return i["phptoId"]
+	return i["photoId"]
 
 def findLoc(placeName):
 	picList = []
@@ -44,6 +47,14 @@ def findDate(dateName):
 			picList.append(i["photoId"])
 	return picList
 
+def addTag(imgId,entity):
+	f = file('photos.json')
+	s = json.load(f)
+	for i in s:
+		if i["photoId"]==imgId:
+			i["tag"]=entity
+	json.dump(s, open('photos.json', 'w'))
+
 def parseJson(s):
 	#f = file('test.json')---------for test
 	#s = json.load(f)-----------for test
@@ -51,18 +62,35 @@ def parseJson(s):
 
 	if res == "show_last":
 		img = lastPic()
+		print img
 	elif res == "show_first":
 		img = firstPic()
+		print img
 	elif res == "show_previous":
 		print "show_previous"
 	elif res == "show_next":
 		print "show next"
 	elif res == "show_n_by_m_per_page":
-		#发送指令到浏览器
+		n=1
+		m=1
+		for ent in s["entities"]:
+			if ent["type"] == "column":
+				n = dict[ent["entity"]]
+			elif ent["type"] == "row":
+				m = dict[ent["entity"]]
+			else:
+				pass
+		print n+"x"+m
 	elif res == "show_n_per_page":
-		#发送指令到浏览器
+		n=1
+		for ent in s["entities"]:
+			if ent["type"] == "count":
+				n = dict[ent["entity"]]
+			else:
+				pass
+		print n
 	elif res == "show_one_per_page":
-		#发送指令到浏览器
+		print "show_one_per_page"
 	elif res == "filter_by_date_location":
 		imglist = allPic()
 		for ent in s["entities"]:
@@ -71,7 +99,7 @@ def parseJson(s):
 			elif ent["type"] == "builtin.datetime.date":
 				imglist = list(set(imglist).intersection(set( findDate(ent["entity"]) )))
 
-	elif res == "filter_by_trip":
+	#elif res == "filter_by_trip":
 
 	elif res == "filter_by_tag":
 		imglist = []
@@ -80,24 +108,31 @@ def parseJson(s):
 				imglist = findTag(ent["entity"])
 
 	elif res == "remove_filter":
-		imglist = allPic()
-		#发送图片到浏览器
+		print "remove_filter"
 	elif res == "add_tag":
-
+		currentImg = "1"#此处需要和浏览器通信，得知当前用户浏览的是哪张图片，得到photoId,是个string
+		for ent in s["entities"]:
+			if ent["type"] == "tag":
+				addTag(currentImg,ent["entity"])
 	elif res == "rotate":
 		if s["entities"][1]["entity"] == "右" or s["entities"][1]["entity"] == "顺时针":
-			#发送指令到浏览器
+			print "right"#发送指令到浏览器
 		elif s["entities"][1]["entity"] == "左" or s["entities"][1]["entity"] == "逆时针":
-			#发送指令到浏览器
+			print "left"#发送指令到浏览器
+		else:
+			pass
 	elif res == "stop_auto_show":
-		#发送指令到浏览器
-	elif res == "start_auto_show":
-		#发送指令到浏览器
-	elif res == "None":
+		print "stop_auto_show"#发送指令到浏览器
 
-	else
+	elif res == "start_auto_show":
+		print "start_auto_show"#发送指令到浏览器
+
+	elif res == "None":
+		print "None"#发送指令到浏览器
+	else:
 		print "not in range"
+
 if __name__ == "__main__":
-    allPic()
+    lastPic()
 
 
